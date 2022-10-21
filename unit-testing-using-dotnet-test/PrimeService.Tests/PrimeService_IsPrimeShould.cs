5eb33b0bb5cf19e89192a8c8f0067187fbf8b84c
charlenefialho <fialhocharlene@gmail.com>
2022-10-21 17:57:17 -0300

    testando XUnit

---
+++ unit-testing-using-dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using PrimeService;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = new PrimeService();
+            bool result = primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+    }
+}
---
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Not fully implemented.");
+        }
+    }
+}
+
+
