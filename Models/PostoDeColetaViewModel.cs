f5bc32870a8467f5325717cb489ec3abc703ab84
charlenefialho <fialhocharlene@gmail.com>
2022-12-06 14:22:28 -0300

    pull

--- Models/PostoDeColetaViewModel.cs
+++ Models/PostoDeColetaViewModel.cs
@@ -11,16 +11,16 @@ namespace ReciGanhaMVC.Models
         public string NomePosto { get; set; }
         public string CNPJ { get; set; }
         public string? EmailPosto { get; set; }
-        public int? NumeroRua{get;set;}
-        public string? Rua {get; set;}
-        public string? Bairro {get;set;}
-        public string? CEP {get;set;}
+        public int NumeroRua{get;set;}
+        public string Rua {get; set;}
+        public string Bairro {get;set;}
+        public string CEP {get;set;}
         public string? TelefonePosto { get; set; }
         public string? WebSite { get; set; }
         #nullable disable
-        public string Latitude { get; set; }
+        public double Latitude { get; set; }
         
-        public string Longitude { get; set; }
+        public double Longitude { get; set; }
         public DateTime DataLimiteExibicao { get; set; }
         public int StatusPosto { get; set; }
         public int[] materiais {get;set;}
